dde52e0248d801449929a91e1256ebb312d663cf
TercanoNuruva <thyoung89@gmail.com>
2024-08-01 09:44:05 -0600

    authenticate

--- server/routes/main.js
+++ server/routes/main.js
@@ -5,3 +5,25 @@ const Admin = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWTSECRET;
+
+//  check for login Middleware
+
+const authMiddleware = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, jwtSecret);
+    req.adminId = decoded.adminId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+// get / Admin - Check Login
+
+router.get();
